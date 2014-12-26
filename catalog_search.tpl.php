0031e50af9e25cfd020729babf2d95d79d72f756
Michael Hagedon <mhagedon@email.arizona.edu>
2014-12-26 15:42:24 -0700

    Initial port of catalog part of Homepage Search to D7. Ref #6950.

---
+++ catalog_search.tpl.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * @file
+ * Template for the catalog search block.
+ */
+?>
+<div class="catalog">
+  <p><?php echo $help_text; ?></p>
+  <div class="form-module form-module--search">
+    <?php echo $catalog_form; ?>
+  </div>
+</div>
