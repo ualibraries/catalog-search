ed780e80f22182ef513e30a4756125e571f3ebcc
Gabriel Luethje <gluethje@email.arizona.edu>
2015-01-13 15:02:34 -0700

    Add a comment to the JS file documenting what it does - refs #6950

--- assets/js/catalog-search.js
+++ assets/js/catalog-search.js
@@ -1,5 +1,17 @@
 (function ($) {
     $(document).ready(function () {
+
+        /**
+         * This function adds a 'submitted' class to
+         * the form when the submit button is clicked.
+         *
+         * We're using this to trigger the animation of
+         * the search icon.
+         *
+         * See line 168 of /assets/css/catalog-search.scss
+         * or line 158 of /assets/css/catalog-search.css
+         */
+
         $('#catalog-search-catalog-form').submit(function () {
             $(this).addClass('submitted');
         });
